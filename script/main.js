77f7e72cb492d5d9c87d93dfea1d21305ffe5dcf
aratislamsafol <aratislamsafol@gmail.com>
2023-02-09 00:25:11 +0600

    Contact,setting,group,Chat all most done

--- script/main.js
+++ script/main.js
@@ -11,3 +11,73 @@ items.forEach(item=>item.addEventListener('click',function(){
         }
     }
 }))
+
+// owl Carousel
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        autoplay:true,
+        margin:10,
+        nav:true,
+        dots: false,
+        navText: [$('.am-next'),$('.am-prev')],
+        responsive:{
+            0:{
+                items:2
+            },
+            300:{
+                items:3
+            },
+            
+            600:{
+                items:5
+            },
+            780:{
+                items:5
+            },
+            1000:{
+                items:7
+            }
+        }
+        })
+});
+
+// Message Box
+const msgBox=document.querySelector('#messageBox');
+const input =document.querySelector(".input input");
+const item =document.querySelector('.offcanvas-footer .container .item');
+input.addEventListener("click",function(){
+    msgBox.classList.toggle('active');
+});
+
+// Message Box for Desktop
+// const msgBox2=document.querySelector('#messageBox2');
+// const input2 =document.querySelector(".input input");
+// const item2 =document.querySelector('.offcanvas-footer .container .item');
+// input2.addEventListener("click",function(){
+//     msgBox2.classList.toggle('active');
+// });
+
+// DropDown btn
+const dropDowns=document.querySelector("#myDropdown");
+function myFunction() {
+    dropDowns.classList.toggle("show");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
+
+// offCanvas Search 
+const search=document.querySelector('.offcanvas-header .searchOffCanvas');
